ae02c31aad06314fab242463dd57c23f4bea9bfe
ncs01060 <ncs01045@gmail.com>
2026-04-02 14:13:22 +0900

    함수 활용해서 별 여러개 출력하기

---
+++ week5/ex3.py
@@ -0,0 +1,12 @@
+# 함수 만들기 연습
+# 함수 선언
+def print_star():
+    print("*" * 30)
+
+print_star()
+print("Hello World!")
+print_star()
+print()
+print_star()
+print("Python Programming Language")
+print_star()
