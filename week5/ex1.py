21e71ffe75782eeb653227fa98519635216e8ee4
ncs01060 <ncs01045@gmail.com>
2026-04-02 13:59:02 +0900

    5주차 함수 전 기초

---
+++ week5/ex1.py
@@ -0,0 +1,34 @@
+# 함수 (function)
+# 커피를 서빙하는 과정
+
+coffee = int(input('메뉴 선택(1. 보통커피 2.설탕커피 3.블랙커피) : '))
+print("#1. 뜨거운 물 준비")
+print("#2. 컵 준비")
+if coffee == 1:
+    print("#3. 보통커피 준비")
+elif coffee == 2:
+    print("#3. 설탕커피 준비")
+elif coffee == 3:
+    print("#3. 블랙커피 준비")
+else:
+    print("#3. 아무거나 준비")
+
+print("#4. 물을 붓는다")
+print("#5. 손님~ 커피 나왔습니다.")
+
+# 두번째 손님
+
+coffee = int(input('메뉴 선택(1. 보통커피 2.설탕커피 3.블랙커피) : '))
+print("#1. 뜨거운 물 준비")
+print("#2. 컵 준비")
+if coffee == 1:
+    print("#3. 보통커피 준비")
+elif coffee == 2:
+    print("#3. 설탕커피 준비")
+elif coffee == 3:
+    print("#3. 블랙커피 준비")
+else:
+    print("#3. 아무거나 준비")
+
+print("#4. 물을 붓는다")
+print("#5. 손님~ 커피 나왔습니다.")
