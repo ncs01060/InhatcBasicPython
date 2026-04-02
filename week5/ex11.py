ca62586ec1ceb4801c7794a6e3a4bd1951e477bf
ncs01060 <ncs01045@gmail.com>
2026-04-02 15:46:47 +0900

    함수 연습 1

---
+++ week5/ex11.py
@@ -0,0 +1,10 @@
+# 함수 연습
+
+def average(n1,n2,n3):
+    return (n1+n2+n3) / 3
+
+num1 = int(input("숫자 1 : "))
+num2 = int(input("숫자 2 : "))
+num3 = int(input("숫자 3 : "))
+
+print("평균 :",average(num1,num2,num3))
